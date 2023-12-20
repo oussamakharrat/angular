b3bac35ee80b5f2bd6bc694ca7b5e6604abc5009
oussamakharrat <oussamapatron37@gmail.com>
2023-12-20 16:40:56 +0100

    aymen

--- src/app/services/store.service.ts
+++ src/app/services/store.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product.model';
 
-const STORE_BASE_URL = 'https://fakestoreapi.com';
+const STORE_BASE_URL = '';
 
 @Injectable({
   providedIn: 'root'
